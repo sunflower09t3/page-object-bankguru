48c7e8cb89dbdef23831179a17492d1025ad7987
DaoThiTuongVi <sunflower09t3@gmail.com>
2019-07-28 22:01:18 +0700

    Add AbstractPageUI class

---
+++ interfaces/bankguru/AbstractPageUI.java
@@ -0,0 +1,19 @@
+package bankguru;
+
+public class AbstractPageUI {
+	public static final String HOME_PAGE_LINK = "//a[text()='Manager']";
+	public static final String NEW_CUSTOMER_LINK = "//a[text()='New Customer']";
+	public static final String EDIT_CUSTOMER_LINK = "";
+	public static final String DELETE_CUSTOMER_LINK = "";
+	public static final String NEW_ACCOUNT_LINK = "";
+	public static final String EDIT_ACCOUNT_LINK = "";
+	public static final String DELETE_ACCOUNT_LINK = "";
+	public static final String DEPOSIT_LINK = "";
+	public static final String WITH_DRAWAL_LINK = "";
+	public static final String FUND_TRANSFER_LINK = "";
+	public static final String CHANGE_PASSWORD_LINK = "";
+	public static final String BALANCE_ENQUIRY_LINK = "";
+	public static final String MINI_STATEMENT_LINK = "";
+	public static final String CUSTOMISED_STATEMENT_LINK = "";
+	public static final String LOGOUT_LINK = "";
+}
