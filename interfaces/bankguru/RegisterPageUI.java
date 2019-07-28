93efeaee380624984d71b9e1e34f1176a69ff192
DaoThiTuongVi <sunflower09t3@gmail.com>
2019-07-28 21:30:58 +0700

    Update RegisterPageUI class

--- interfaces/bankguru/RegisterPageUI.java
+++ interfaces/bankguru/RegisterPageUI.java
@@ -1,5 +1,8 @@
 package bankguru;
 
 public class RegisterPageUI {
-
+	public static final String EMAIL_ID_TEXTBOX = "//*[@name='emailid']";
+	public static final String SUBMIT_BUTTON = "//*[@name='btnLogin']";
+	public static final String USER_ID_TEXT = "//td[contains(.,'User ID')]/following-sibling::td";
+	public static final String PASSWORD_TEXT = "//td[contains(.,'Password')]/following-sibling::td";
 }
