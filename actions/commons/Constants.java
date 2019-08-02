4f84990406629d27b90d2c4d9c757ab7018772dc
DaoThiTuongVi <sunflower09t3@gmail.com>
2019-08-02 11:00:21 +0700

    Define a few constants in Constants class

--- actions/commons/Constants.java
+++ actions/commons/Constants.java
@@ -1,5 +1,11 @@
 package commons;
 
 public class Constants {
-
+	public static final int LONG_TIMEOUT = 30;
+	public static final int SHORT_TIMEOUT = 10;
+	public static final String CHROME_HEADLESS_WINDOWS_SIZE = "1366x768";
+	public static final String CHROME_DRIVER_PATH = ".\\resources\\chromedriver.exe";
+	public static final String DEV_URL = "http://demo.guru99.com/V4/";
+	public static final String DEV_SQL_USERNAME = "mngr213678";
+	public static final String DEV_SQL_PASSWORD = "Anajabu";
 }
