0afa78b232ab517cc5f2bef412905504e885bb74
DaoThiTuongVi <sunflower09t3@gmail.com>
2019-08-03 17:20:17 +0700

    Set SHORT_TIMEOUT constant to 5

--- actions/commons/Constants.java
+++ actions/commons/Constants.java
@@ -2,7 +2,7 @@ package commons;
 
 public class Constants {
 	public static final int LONG_TIMEOUT = 30;
-	public static final int SHORT_TIMEOUT = 10;
+	public static final int SHORT_TIMEOUT = 5;
 	public static final String CHROME_HEADLESS_WINDOWS_SIZE = "1366x768";
 	public static final String CHROME_DRIVER_PATH = ".\\resources\\chromedriver.exe";
 	public static final String DEV_URL = "http://demo.guru99.com/V4/";
