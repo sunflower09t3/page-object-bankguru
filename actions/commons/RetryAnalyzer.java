67ab3062136c62517847599b3bb700568ab8e586
DaoThiTuongVi <sunflower09t3@gmail.com>
2019-08-26 16:05:30 +0700

    Retry failed test cases

---
+++ actions/commons/RetryAnalyzer.java
@@ -0,0 +1,22 @@
+package commons;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer{
+	private int retryCounter = 0;
+	private int retryLimit = 2;
+	
+	@Override
+	public boolean retry(ITestResult result) {
+		if(retryCounter < retryLimit) {
+			retryCounter++;
+			System.out.println("Retry test name: " + result.getTestName() + " with: " + retryCounter + "times");
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+}
